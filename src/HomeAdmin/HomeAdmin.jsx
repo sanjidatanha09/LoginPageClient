16dcafbdf8a4ca577a957ea2436c932317e056a8
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-15 17:29:54 +0600

    another input field

--- src/HomeAdmin/HomeAdmin.jsx
+++ src/HomeAdmin/HomeAdmin.jsx
@@ -4,7 +4,13 @@ const HomeAdmin = () => {
     return (
         <div>
             <h1>enter your name</h1>
-            <input type="text" />
+            <input type="text" placeholder='type here name'/>
+            <h1>enter your age</h1>
+            <input type="text" placeholder='type here name' />
+            <h1>enter your mobile</h1>
+            <input type="text" placeholder='type here name' />
+            <h1>enter your phone number</h1>
+            <input type="text" placeholder='type here name' />
         </div>
     );
 };
